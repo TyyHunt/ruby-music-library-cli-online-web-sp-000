bc7abc7cfc5543a3a51ed09d280cd2bd95aeb264
Tyler Hunt <tyhunt04@gmail.com>
2019-03-03 00:58:26 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -100,7 +100,7 @@ class MusicLibraryController
         when "list genre"
           self.list_songs_by_genre
         when "play song"
-          self.play_song
+          self.play_song(arg)
         end
       end
     end
